f5ef0bfa463980d03a6bcda3fbb1a51be3af5ec8
warnerb47 <taphagaye2001.mg@gmail.com>
2022-09-28 20:35:38 +0000

    feat: add pets ressources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PetsModule
+  ],
   providers: [AppService],
 })
 export class AppModule {}
